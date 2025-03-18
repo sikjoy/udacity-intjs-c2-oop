27195121b233f72d991584f35b16481f0a96db22
Kevin Mathis <kfmathis@gmail.com>
2025-03-17 20:40:45 -0700

    get form data

--- app.js
+++ app.js
@@ -8,6 +8,41 @@
     // Create Human Object
 
     // Use IIFE to get human data from form
+    const form = (() => {
+      const nameElement = document.getElementById('name');
+      const feetElement = document.getElementById('feet');
+      const inchesElement = document.getElementById('inches');
+      const weightElement = document.getElementById('weight');
+      const dietElement = document.getElementById('diet');
+
+      const getName = () => {
+        return nameElement.value;
+      };
+
+      const getFeet = () => {
+        return feetElement.value;
+      };
+
+      const getInches = () => {
+        return inchesElement.value;
+      };
+
+      const getWeight = () => {
+        return weightElement.value;
+      };
+
+      const getDiet = () => {
+        return dietElement.value;
+      };
+
+      return {
+        getName: getName,
+        getFeet: getFeet,
+        getInches: getInches,
+        getWeight: getWeight,
+        getDiet: getDiet,
+      };
+    })();
 
 
     // Create Dino Compare Method 1
@@ -30,3 +65,11 @@
 
 
 // On button click, prepare and display infographic
+const button = document.getElementById('btn');
+button.addEventListener('click', (event) => {
+  console.log(form.getName());
+  console.log(form.getFeet());
+  console.log(form.getInches());
+  console.log(form.getWeight());
+  console.log(form.getDiet());
+});
